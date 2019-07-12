4e2e90f39c65a131d227feedf4902612d205b9f0
Aibar01 <bekaybar@gmail.com>
2019-07-12 22:46:07 +0600

    change DEBUG to False

--- scaf/settings.py
+++ scaf/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'scaffolding',
         'USER': 'scaf',
-        'PASSWORD': 'A4655462340a',
+        'PASSWORD': 'A4655462340',
         'HOST': 'localhost',
         'PORT': '',
     }
