cbe4d41319073af67af096b0dec28fec1ba8e1ea
Aibar01 <bekaybar@gmail.com>
2019-07-12 22:41:57 +0600

    change to postgresql

--- scaf/settings.py
+++ scaf/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'scaf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'scaffolding',
+        'USER': 'scaf',
+        'PASSWORD': 'A4655462340a',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
