1b4cb5a829f18191c63d1d8130807b0dd510f921
Aibar01 <bekaybar@gmail.com>
2019-07-17 22:19:04 +0600

    add big size images in settings

--- scaf/settings.py
+++ scaf/settings.py
@@ -117,6 +117,8 @@ USE_L10N = True
 USE_TZ = True
 
 
+FILE_UPLOAD_MAX_MEMORY_SIZE = 200000000
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
